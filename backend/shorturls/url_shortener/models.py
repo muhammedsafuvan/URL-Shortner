7cedea939f5e2798443bfdfa90ffee0323f6c30d
muhammedsafuvan <muhammedsafuvan024@gmail.com>
2024-03-03 21:35:07 +0530

    update model

--- backend/shorturls/url_shortener/models.py
+++ backend/shorturls/url_shortener/models.py
@@ -5,6 +5,7 @@ class ShortURL(models.Model):
     original_url = models.URLField()
     short_code = models.CharField(max_length=20, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
+    is_valid = True
 
     def __str__(self):
         return self.original_url
