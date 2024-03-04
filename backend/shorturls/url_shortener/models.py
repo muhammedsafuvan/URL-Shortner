dfac51846e5e72242a6af2e3cc7968efe27e12f1
muhammedsafuvan <muhammedsafuvan024@gmail.com>
2024-03-04 12:59:37 +0530

    [SU-5] Update databse to postgresql

--- backend/shorturls/url_shortener/models.py
+++ backend/shorturls/url_shortener/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 
 
-class Urls(models.Model):
+class Url(models.Model):
     original_url = models.URLField()
     short_code = models.CharField(max_length=20, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
